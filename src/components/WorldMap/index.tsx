139f06d1ddcb64a079508be60af6920cabdf32ef
jong-k <fully.charged07@gmail.com>
2023-10-21 17:31:52 +0900

    feat: 간단한 페이지 추가

---
+++ src/components/WorldMap/index.tsx
@@ -0,0 +1,7 @@
+export default function WorldMap() {
+  return (
+    <div>
+      <h1>World Map</h1>
+    </div>
+  );
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import Sample from "./components/Sample";
+import Map from "./components/WorldMap";
 
 export const router = createBrowserRouter([
   {
@@ -13,6 +14,6 @@ export const router = createBrowserRouter([
   },
   {
     path: "/map",
-    element: <div></div>,
+    element: <Map />,
   },
 ]);
