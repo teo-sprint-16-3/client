448959532f39400dc7dac5cb66219cf33282e137
DDUDII <qortn121@naver.com>
2023-10-21 18:09:15 +0900

    init: first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { NavLink } from "react-router-dom";
+import "./styles/app.scss";
 
 export default function App() {
   return (
-    <>
-      <h2>Hello from App</h2>
+    <div className="app">
+      <header>
+        <h1> 세계 여행 어디까지 가봤니? </h1>
+      </header>
       <NavLink to="/sample">Sample Page</NavLink>
-    </>
+    </div>
   );
 }
