b831a21ea567204b54759d4ace64d55522e765c3
jong-k <fully.charged07@gmail.com>
2023-10-21 22:27:12 +0900

    temp commit

--- src/components/WorldMap/index.tsx
+++ src/components/WorldMap/index.tsx
@@ -1,57 +1,9 @@
+// import { ComposableMap, Geographies, Geography } from "react-simple-maps";
+
 export default function WorldMap() {
   return (
     <div>
       <h1>World Map</h1>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
-      <p>haha</p>
     </div>
   );
 }
