23de81cc4b309832567aa379a33b3f81eb731627
sukam09 <sukam09@naver.com>
2023-10-21 17:59:59 +0900

    feat: note 라우터 생성

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import Sample from "./components/Sample";
+import { Note } from "./pages/Note";
 
 export const router = createBrowserRouter([
   {
@@ -10,5 +11,9 @@ export const router = createBrowserRouter([
   {
     path: "/sample",
     element: <Sample />,
+  },
+  {
+    path: "/note",
+    element: <Note />,
   },
 ]);
