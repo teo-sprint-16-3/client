6ed1d30a1f79218452371d6c84c1a317eca3f82b
jong-k <fully.charged07@gmail.com>
2023-10-21 23:59:44 +0900

    feat: hover 시 컬러 변경 기능 추가

--- src/components/WorldMap/index.tsx
+++ src/components/WorldMap/index.tsx
@@ -1,14 +1,18 @@
 import { ComposableMap, Geographies, Geography } from "react-simple-maps";
-import "./index.module.scss";
+import s from "./index.module.scss";
 
 export default function WorldMap() {
   return (
-    <div>
-      <ComposableMap>
+    <div className={s.wrapper}>
+      <ComposableMap className={s.mapBox}>
         <Geographies geography="/features.json">
           {({ geographies }) =>
             geographies.map((geo) => (
-              <Geography key={geo.rsmKey} geography={geo} />
+              <Geography
+                key={geo.rsmKey}
+                geography={geo}
+                className={s.mapGeography}
+              />
             ))
           }
         </Geographies>
