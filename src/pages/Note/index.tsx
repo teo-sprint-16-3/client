a72622f18665428e99dcc33972db59d48686557e
sukam09 <sukam09@naver.com>
2023-10-22 06:27:26 +0900

    feat: 뒷배경 추가

--- src/pages/Note/index.tsx
+++ src/pages/Note/index.tsx
@@ -1,7 +1,9 @@
 import { Header } from "../../components/Note/Header";
+import { CategoryTab } from "../../components/Note/CategoryTab";
+import { SortingTab } from "../../components/Note/SortingTab";
 import { TripGrid } from "../../components/Note/TripGrid";
 import { WriteButton } from "../../components/Note/common/WriteButton";
-import { Navbar } from "../../components/Note/Navbar";
+import GNB from "../../components/GNB";
 
 import s from "./index.module.scss";
 
@@ -9,9 +11,13 @@ export function Note() {
   return (
     <div className={s.container}>
       <Header />
-      <TripGrid />
-      <WriteButton />
-      {/* <Navbar /> */}
+      <CategoryTab />
+      <div className={s.wrapper}>
+        <SortingTab />
+        {/* <TripGrid /> */}
+        {/* <WriteButton /> */}
+      </div>
+      {/* <GNB /> */}
     </div>
   );
 }
