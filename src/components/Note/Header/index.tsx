7c65fc9bba04bf1c0812e34afdf7665bc0e14203
sukam09 <sukam09@naver.com>
2023-10-21 23:33:22 +0900

    feat: 헤더에서 필터 제거

--- src/components/Note/Header/index.tsx
+++ src/components/Note/Header/index.tsx
@@ -3,8 +3,7 @@ import s from "./index.module.scss";
 export function Header() {
   return (
     <div className={s.container}>
-      <span className={s.note}>노트</span>
-      <span className={s.filter}>필터</span>
+      <div className={s.title}>노트</div>
     </div>
   );
 }
