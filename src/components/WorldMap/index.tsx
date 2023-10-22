447eafcd0936c3b5749856c6cf6835014663fb37
jong-k <fully.charged07@gmail.com>
2023-10-22 16:05:25 +0900

    feat: 시작 위치를 한국 위도경도로 설정

--- src/components/WorldMap/index.tsx
+++ src/components/WorldMap/index.tsx
@@ -6,11 +6,13 @@ import {
 } from "react-simple-maps";
 import s from "./index.module.scss";
 
+const KOREA_COORD = [129, 37] as [number, number];
+
 export default function WorldMap() {
   return (
     <div className={s.wrapper}>
       <ComposableMap className={s.mapBox}>
-        <ZoomableGroup zoom={9}>
+        <ZoomableGroup center={KOREA_COORD} zoom={9}>
           <Geographies geography="/features.json">
             {({ geographies }) =>
               geographies.map((geo) => {
