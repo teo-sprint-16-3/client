5fae153ae82743ab3cdb497f8a27dedac7a41733
sukam09 <sukam09@naver.com>
2023-10-23 02:40:56 +0900

    feat: mock data 생성 및 정렬 기능 구현

--- src/components/Note/NoteList/index.tsx
+++ src/components/Note/NoteList/index.tsx
@@ -1,28 +1,98 @@
-import s from "./index.module.scss";
+import { useState, useEffect } from "react";
 
 import { NoteSummary } from "./NoteSummary";
 import { CountrySummary } from "./CountrySummary";
 import { YearSummary } from "./YearSummary";
 
+import s from "./index.module.scss";
+import { get } from "http";
+
 interface Props {
   category: string;
+  sortingOrder: string;
 }
 
-export function NoteList({ category }: Props) {
+// 이거 NoteSummary의 Props와 type 겹치는데 원래는 별도의 types 폴더에 빼는게 좋음
+// 지금은 시간이 없으므로 그냥 함
+interface DataProps {
+  year: number;
+  country: string;
+  flag: string;
+  title: string;
+  date: string;
+  location?: string;
+  bgm?: string;
+  image?: string;
+}
+
+const mockData = [
+  {
+    year: 2023,
+    country: "몽골",
+    flag: "/src/assets/countryFlags/Mongolia.svg",
+    title: "몽골 8일차🐴 말타고 초원 달린 날",
+    date: "2023.07.15",
+    location: "욜링암 협곡 사이",
+    bgm: "BOL4 - TRAVEL",
+    image: "/src/assets/images/note1.png",
+  },
+  {
+    year: 2023,
+    country: "몽골",
+    flag: "/src/assets/countryFlags/Mongolia.svg",
+    title: "몽골 강아지들 진짜 귀엽다🐕 왕크왕귀",
+    date: "2023.07.14",
+    image: "/src/assets/images/note1.png",
+  },
+  {
+    year: 2023,
+    country: "대한민국",
+    flag: "/src/assets/countryFlags/South Korea.svg",
+    title: "넘넘 즐거웠던 경주여행 3",
+    date: "2022.05.08-2023.05.15",
+  },
+];
+
+function getStartDate(date: string) {
+  return date.split("-")[0];
+}
+
+function compareDate(date1: Date, date2: Date, isAscending: boolean) {
+  if (date1 < date2) {
+    return isAscending ? -1 : 1;
+  } else if (date1 > date2) {
+    return isAscending ? 1 : -1;
+  } else {
+    return 0;
+  }
+}
+
+export function NoteList({ category, sortingOrder }: Props) {
+  const [data, setData] = useState<DataProps[]>([]);
+
+  function getNoteSummary() {
+    const data = mockData.slice();
+
+    data.sort((a, b) => {
+      const date1 = new Date(getStartDate(a.date));
+      const date2 = new Date(getStartDate(b.date));
+
+      return compareDate(date1, date2, sortingOrder === "ascending");
+    });
+
+    return data;
+  }
+
+  useEffect(() => {
+    const data = getNoteSummary();
+    setData(data);
+  }, [category, sortingOrder]);
+
   return (
     <div className={s.container}>
-      {category === "all" && (
-        <NoteSummary
-          year={2023}
-          country="몽골"
-          flag="/src/assets/icons/mongolian-state.png"
-          title="몽골 8일차🐴 말타고 초원 달린 날"
-          date="2023.07.15"
-          location="욜링암 협곡 사이"
-          bgm="BOL4 - TRAVEL"
-          image="/src/assets/images/note1.png"
-        />
-      )}
+      {category === "all" &&
+        data.map(({ ...props }) => <NoteSummary {...props} />)}
+
       {category === "country" && (
         <CountrySummary
           flag="/src/assets/icons/mongolian-state.png"
