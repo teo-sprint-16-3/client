36699529b69e9876135eb254edd7575d995b4d1a
ohseong_kim <bkks1004@naver.com>
2023-10-22 21:31:00 +0900

    feat: Stamp header 디자인 적용

--- src/components/Stamp/Header/index.tsx
+++ src/components/Stamp/Header/index.tsx
@@ -1,9 +1,13 @@
+import Back from "../../../assets/icons/back.svg";
+import More from "../../../assets/icons/more.svg";
 import styles from "./index.module.scss";
 
 export default function Header() {
   return (
     <div className={styles.header}>
-      <h1>여행 국가</h1>
+      <img src={Back} alt="back" />
+      <div className={styles.title}>여행 국가</div>
+      <img src={More} alt="more" />
     </div>
   );
 }
