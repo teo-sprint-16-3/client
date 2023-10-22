a5066b7b925424c7265c5239810a7056f3093eb5
osdoonhyun <skw1996@naver.com>
2023-10-23 05:29:31 +0900

    feat: 쓰기 페이지 react-hook-form 적용 (제출, yup 기능 제외)

--- src/pages/note/CreateNotePage/index.tsx
+++ src/pages/note/CreateNotePage/index.tsx
@@ -7,14 +7,53 @@ import PictureInput from "../../../components/Note/PictureInput";
 import TextInput from "../../../components/Note/TextInput";
 import TitleInput from "../../../components/Note/TitleInput";
 import Header from "../../../components/Note/common/Header";
-import { useState } from "react";
+import { useState, KeyboardEvent } from "react";
 import BottomSheet from "../../../components/Note/common/BottomSheet";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 import { produce } from "immer";
+
+import * as yup from "yup";
+import CountryInput from "../../../components/Note/CountryInput";
+
+interface NoteInputForm {
+  title: string;
+  // date: { startDate: string; endDate: string };
+  // country: { name: string; flag?: string };
+  location?: string;
+  bgm?: string;
+  images?: string[];
+  description?: string;
+}
+
+const notePostSchema = yup.object({
+  // title: yup.string().required("제목을 입력해주세요."),
+  // date: yup
+  //   .object({
+  //     startDate: yup.string(),
+  //     endDate: yup.string(),
+  //   })
+  //   .test("start-end-date", "날짜를 선택해 주세요.", function (value) {
+  //     const { startDate, endDate } = value;
+  //     return startDate && endDate;
+  //   }),
+  // country: yup.object({
+  //   name: yup.string().required("국가를 선택해 주세요."),
+  //   flag: yup.string(),
+  // }),
+  // location: yup.string().max(30, "최대 30글자까지 입력 가능합니다."),
+});
 
 export default function CreateNotePage() {
   const [isBottomSheetOpen, setIsBottomSheetOpen] = useState(false);
   const [bottomSheetContent, setBottomSheetContent] = useState(null);
   const [fileUrls, setFileUrls] = useState<string[]>(["", "", ""]);
+
+  const useFormReturn = useForm({
+    resolver: yupResolver(notePostSchema),
+    mode: "onSubmit",
+    // defaultValues: {},
+  });
 
   const handleBottomSheetOpen = (content: any): void => {
     setBottomSheetContent(content);
@@ -33,17 +72,55 @@ export default function CreateNotePage() {
     );
   };
 
+  const handleNoteSubmit: SubmitHandler<NoteInputForm> = (data) => {
+    const filteredFileUrls = fileUrls.filter((file) => file !== "");
+    console.log("@@@", filteredFileUrls);
+    const noteData = {
+      title: data.title,
+      // date: { startDate: data.date.startDate, endDate: data.date.endDate },
+      // country: { name: data.country.name, flag: data.country.flag },
+      // location: data.location ?? "",
+      bgm: data.bgm ?? "",
+      images: filteredFileUrls ?? [],
+      description: data.description ?? "",
+    };
+
+    console.log("제출완료", noteData);
+    // if(isEdit){
+    //   updateNoteSubmit(noteData)
+    // } else {
+    //   postNoteSubmit(noteData)
+    // }
+  };
+
+  const onKeyDownSubmit = (event: KeyboardEvent<HTMLFormElement>) => {
+    const { id } = event.target as Element;
+    if (event.key === "Enter") {
+      if (id === "desk-textarea") {
+        return;
+      }
+
+      event.preventDefault();
+    }
+  };
+
   return (
     <>
       <Container isBottomSheetOpen={isBottomSheetOpen}>
-        <Header />
-        <TitleInput />
-        <DateInput onSelectCountry={handleBottomSheetOpen} />
-        <LocationInput onSelectCountry={handleBottomSheetOpen} />
-        <BgmInput />
-        <PictureInput fileUrls={fileUrls} onChangeFile={onChangeFileUrls} />
-        <TextInput />
-        <SubmitButton />
+        <form
+          onKeyDown={onKeyDownSubmit}
+          onSubmit={useFormReturn.handleSubmit(handleNoteSubmit)}
+        >
+          <Header />
+          <TitleInput useForm={useFormReturn} />
+          <DateInput onSelectDate={handleBottomSheetOpen} />
+          <CountryInput onSelectCountry={handleBottomSheetOpen} />
+          <LocationInput useForm={useFormReturn} />
+          <BgmInput onSearchBgm={handleBottomSheetOpen} />
+          <PictureInput fileUrls={fileUrls} onChangeFile={onChangeFileUrls} />
+          <TextInput useForm={useFormReturn} />
+          <SubmitButton />
+        </form>
       </Container>
 
       <div>
