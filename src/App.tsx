1c4dabe06220f6c182142e5e0e0797e1d0c35d7d
jong-k <fully.charged07@gmail.com>
2023-10-22 23:25:00 +0900

    temp commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import DefaultLayout from "./components/DefaultLayout";
+import southKorea from "./assets/countryFlags/South Korea.svg";
 
 export default function App() {
   return (
     <DefaultLayout>
+      <img src={southKorea} alt="" />
       <h2>Hello from App</h2>
       <h2>Hello from App</h2>
       <h2>Hello from App</h2>
