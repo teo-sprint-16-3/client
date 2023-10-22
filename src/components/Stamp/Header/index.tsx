1380d2e50dd78f10f007e91282166e7a07557fba
ohseong_kim <bkks1004@naver.com>
2023-10-22 22:24:46 +0900

    feat: Stamp back 적용

--- src/components/Stamp/Header/index.tsx
+++ src/components/Stamp/Header/index.tsx
@@ -1,11 +1,16 @@
+import { useNavigate } from "react-router-dom";
 import Back from "../../../assets/icons/back.svg";
 import More from "../../../assets/icons/more.svg";
 import styles from "./index.module.scss";
 
 export default function Header() {
+  const navigate = useNavigate();
+  const goBack = () => {
+    navigate(-1);
+  };
   return (
     <div className={styles.header}>
-      <img src={Back} alt="back" />
+      <img src={Back} alt="back" className={styles.back} onClick={goBack} />
       <div className={styles.title}>여행 국가</div>
       <img src={More} alt="more" />
     </div>
