adb54a872cf0212e4024441af97dcf24284f8a1d
jong-k <fully.charged07@gmail.com>
2023-10-23 00:46:09 +0900

    feat: 인트로 페이지 마크업 완성

--- src/components/Intro/index.tsx
+++ src/components/Intro/index.tsx
@@ -1,9 +1,19 @@
+// import { useNavigate } from "react-router-dom";
 import introLogo from "../../assets/icons/introLogo.svg";
+import s from "./index.module.scss";
 
 export default function Intro() {
   return (
-    <div>
-      <img src={introLogo} alt="세어봤" />
+    <div className={s.wrapper}>
+      <div className={s.logoBox}>
+        <img className={s.introLogo} src={introLogo} alt="세어봤" />
+      </div>
+      <div className={s.btnBox}>
+        <button className={[s.btn, s.loginBtn].join(" ")}>SNS 로그인</button>
+        <button className={[s.btn, s.exploreBtn].join(" ")}>
+          <span>둘러보기</span>
+        </button>
+      </div>
     </div>
   );
 }
