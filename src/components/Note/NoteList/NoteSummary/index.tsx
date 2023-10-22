4e4f5f80b07be7fb055e3d137e1ed1a1b963a7fd
sukam09 <sukam09@naver.com>
2023-10-22 10:29:48 +0900

    feat: 노트 서머리 버튼 구현

---
+++ src/components/Note/NoteList/NoteSummary/index.tsx
@@ -0,0 +1,63 @@
+import calendarIcon from "../../../../assets/icons/calendar.svg";
+import locationIcon from "../../../../assets/icons/location.svg";
+import bgmIcon from "../../../../assets/icons/music.svg";
+
+import s from "./index.module.scss";
+
+interface Props {
+  year: number;
+  country: string;
+  flag: string;
+  title: string;
+  date: string;
+  location?: string;
+  bgm?: string;
+  image?: string;
+}
+
+export function NoteSummary({
+  year,
+  country,
+  flag,
+  title,
+  date,
+  location,
+  bgm,
+  image,
+}: Props) {
+  return (
+    <div className={s.container}>
+      <div className={s.titleWrapper}>
+        <div className={s.tagWrapper}>
+          <div className={`${s.tag} ${s.year}`}>{year}</div>
+          <div className={`${s.tag} ${s.country}`}>{country}</div>
+          <img className={s.flag} src={flag} alt="국기 이미지" />
+        </div>
+        <div className={s.title}>{title}</div>
+      </div>
+      <div className={s.contentWrapper}>
+        <div className={s.descriptionWrapper}>
+          <div className={s.descriptionBox}>
+            <img src={calendarIcon} alt="달력 이미지" />
+            <div className={s.description}>{date}</div>
+          </div>
+          {location !== undefined && (
+            <div className={s.descriptionBox}>
+              <img src={locationIcon} alt="위치 이미지" />
+              <div className={s.description}>{location}</div>
+            </div>
+          )}
+          {bgm !== undefined && (
+            <div className={s.descriptionBox}>
+              <img src={bgmIcon} alt="음악 이미지" />
+              <div className={s.description}>{bgm}</div>
+            </div>
+          )}
+        </div>
+        {image !== undefined && (
+          <img className={s.image} src={image} alt="여행 이미지" />
+        )}
+      </div>
+    </div>
+  );
+}
