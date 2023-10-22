f7efb56bf7bf72fb01f226916f77af4bad477f4f
jong-k <fully.charged07@gmail.com>
2023-10-22 22:06:26 +0900

    feat: pin click 시 리코일에 name 저장 기능 추가

--- src/components/WorldMap/index.tsx
+++ src/components/WorldMap/index.tsx
@@ -1,28 +1,66 @@
+import { useState } from "react";
 import {
   ComposableMap,
   Geographies,
   Geography,
   ZoomableGroup,
 } from "react-simple-maps";
+import { useRecoilState } from "recoil";
 import s from "./index.module.scss";
+import { countryNameState } from "../../recoil/country/atom.ts";
+import { isSavedName } from "../../utils";
 
 const KOREA_COORD = [129, 37] as [number, number];
 
+interface ClickedPosType {
+  y: number;
+  x: number;
+}
+
 export default function WorldMap() {
+  const [countryNameList, setCountryNameList] =
+    useRecoilState(countryNameState);
+  const [currentCountry, setCurrentCountry] = useState<string>("");
+  const [clickedPos, setClickedPos] = useState<ClickedPosType>({
+    y: 0,
+    x: 0,
+  });
+  const [pointBoxOn, setPointBoxOn] = useState(false);
+  const handleClickCountry = (e: React.MouseEvent, name: string) => {
+    e.stopPropagation();
+    setCurrentCountry(name);
+    setClickedPos({
+      y: e.nativeEvent.offsetY,
+      x: e.nativeEvent.offsetX,
+    });
+    setPointBoxOn(true);
+  };
+
+  const countryNameSave = (name: string, korName: string) => {
+    if (!isSavedName(name, countryNameList)) {
+      setCountryNameList((currVal) => [...currVal, { name, korName }]);
+    }
+  };
+
   return (
     <div className={s.wrapper}>
       <ComposableMap className={s.mapBox}>
-        <ZoomableGroup center={KOREA_COORD} zoom={7}>
+        <ZoomableGroup center={KOREA_COORD} zoom={7} maxZoom={1000}>
           <Geographies geography="/features.json">
             {({ geographies }) =>
               geographies.map((geo) => {
-                const name = geo.properties.name;
+                const countryName = geo.properties.name;
+                const isClicked = currentCountry === countryName;
+
                 return (
                   <Geography
-                    onClick={() => console.log(name)}
+                    onClick={(e) => handleClickCountry(e, countryName)}
                     key={geo.rsmKey}
                     geography={geo}
-                    className={s.country}
+                    className={[
+                      s.country,
+                      isClicked ? s.clickedCountry : "",
+                    ].join(" ")}
                     stroke="#444"
                     strokeWidth={0.05}
                     style={{
@@ -36,6 +74,18 @@ export default function WorldMap() {
           </Geographies>
         </ZoomableGroup>
       </ComposableMap>
+      {pointBoxOn && (
+        <div
+          className={s.popup}
+          style={{
+            position: "absolute",
+            left: clickedPos.x + 1 + "px",
+            top: clickedPos.y + 1 + "px",
+          }}
+        >
+          <p>{currentCountry}</p>
+        </div>
+      )}
     </div>
   );
 }
